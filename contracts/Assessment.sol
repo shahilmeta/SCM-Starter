e3265f72e46fda74eb018962b95f005c5f76da75
SURYAJIT ITHIPARAMBIL S <vas20cs117@vidyaacademy.ac.in>
2024-01-19 08:34:24 +0000

    final

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,60 +1,78 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
-    address payable public owner;
-    uint256 public balance;
+    address public owner;
+    mapping(address => uint256) public balance;
+    mapping(address => string) public ownerNames; // New mapping for owner names
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event Transfer(address recipient, uint256 amount);
 
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
-    }
-
-    function getBalance() public view returns(uint256){
-        return balance;
-    }
-
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
+    modifier onlyOwner() {
         require(msg.sender == owner, "You are not the owner of this account");
+        _;
+    }
 
-        // perform transaction
-        balance += _amount;
+    constructor(uint256 initBalance) payable {
+        owner = msg.sender;
+        balance[owner] = initBalance;
+    }
 
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
+    function setOwnerName(string memory _ownerName) external onlyOwner {
+        ownerNames[owner] = _ownerName;
+    }
 
-        // emit the event
+    function getBalance() external view returns (uint256) {
+        return balance[owner];
+    }
+
+    function getOwnerName() external view returns (string memory) {
+        return ownerNames[owner];
+    }
+
+    function deposit(uint256 _amount) external payable onlyOwner {
+        uint256 _previousBalance = balance[owner];
+
+        // Perform transaction
+        balance[owner] += _amount;
+
+        // Assert transaction completed successfully
+        assert(balance[owner] == _previousBalance + _amount);
+
+        // Emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
+    // Custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
+    function withdraw(uint256 _withdrawAmount) external onlyOwner {
+        uint256 _previousBalance = balance[owner];
+        if (balance[owner] < _withdrawAmount) {
+            revert InsufficientBalance({ balance: balance[owner], withdrawAmount: _withdrawAmount });
         }
 
-        // withdraw the given amount
-        balance -= _withdrawAmount;
+        // Withdraw the given amount
+        balance[owner] -= _withdrawAmount;
 
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
+        // Assert the balance is correct
+        assert(balance[owner] == (_previousBalance - _withdrawAmount));
 
-        // emit the event
+        // Emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function transfer(address _recipient, uint256 _amount) external onlyOwner {
+        require(_amount <= balance[owner], "Insufficient balance");
+
+        // Transfer the specified amount to the recipient
+        balance[_recipient] += _amount;
+        // Update the balance
+        balance[owner] -= _amount;
+
+        // Emit the event
+        emit Transfer(_recipient, _amount);
     }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import {useState, useEffect} from "react";
-import {ethers} from "ethers";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
 
 export default function HomePage() {
@@ -7,41 +7,42 @@ export default function HomePage() {
   const [account, setAccount] = useState(undefined);
   const [atm, setATM] = useState(undefined);
   const [balance, setBalance] = useState(undefined);
+  const [owner, setOwner] = useState("");
+  const [recipient, setRecipient] = useState("");
+  const [amount, setAmount] = useState(1);
 
-  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const contractAddress = "0xdc64a140aa3e981100a9beca4e685f962f0cf6c9";
   const atmABI = atm_abi.abi;
 
-  const getWallet = async() => {
+  const getWallet = async () => {
     if (window.ethereum) {
       setEthWallet(window.ethereum);
     }
 
     if (ethWallet) {
-      const account = await ethWallet.request({method: "eth_accounts"});
-      handleAccount(account);
+      const accounts = await ethWallet.request({ method: "eth_accounts" });
+      handleAccount(accounts[0]);
     }
-  }
+  };
 
   const handleAccount = (account) => {
     if (account) {
-      console.log ("Account connected: ", account);
+      console.log("Account connected:", account);
       setAccount(account);
-    }
-    else {
+    } else {
       console.log("No account found");
     }
-  }
+  };
 
-  const connectAccount = async() => {
+  const connectAccount = async () => {
     if (!ethWallet) {
-      alert('MetaMask wallet is required to connect');
+      alert("MetaMask wallet is required to connect");
       return;
     }
-  
-    const accounts = await ethWallet.request({ method: 'eth_requestAccounts' });
-    handleAccount(accounts);
-    
-    // once wallet is set we can get a reference to our deployed contract
+
+    const accounts = await ethWallet.request({ method: "eth_requestAccounts" });
+    handleAccount(accounts[0]);
+
     getATMContract();
   };
 
@@ -49,45 +50,71 @@ export default function HomePage() {
     const provider = new ethers.providers.Web3Provider(ethWallet);
     const signer = provider.getSigner();
     const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
- 
-    setATM(atmContract);
-  }
 
-  const getBalance = async() => {
+    setATM(atmContract);
+    getOwnerName();
+  };
+
+  const getBalance = async () => {
     if (atm) {
       setBalance((await atm.getBalance()).toNumber());
     }
-  }
+  };
 
-  const deposit = async() => {
+  const getOwnerName = async () => {
+    if (atm) {
+      setOwner(await atm.getOwnerName());
+    }
+  };
+
+  const deposit = async () => {
     if (atm) {
       let tx = await atm.deposit(1);
-      await tx.wait()
+      await tx.wait();
       getBalance();
     }
-  }
+  };
 
-  const withdraw = async() => {
+  const withdraw = async () => {
     if (atm) {
       let tx = await atm.withdraw(1);
-      await tx.wait()
+      await tx.wait();
       getBalance();
     }
-  }
+  };
+
+  const transfer = async () => {
+    if (atm) {
+      if (!recipient || !amount) {
+        console.error("Recipient address and amount are required for transfer.");
+        return;
+      }
+
+      let tx = await atm.transfer(recipient, amount);
+      await tx.wait();
+      getBalance();
+    }
+  };
 
   const initUser = () => {
-    // Check to see if user has Metamask
     if (!ethWallet) {
-      return <p>Please install Metamask in order to use this ATM.</p>
+      return <p>Please install Metamask in order to use this ATM.</p>;
     }
 
-    // Check to see if user is connected. If not, connect to their account
     if (!account) {
-      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>
+      return (
+        <button onClick={connectAccount}>
+          Please connect your Metamask wallet
+        </button>
+      );
     }
 
-    if (balance == undefined) {
+    if (balance === undefined) {
       getBalance();
+    }
+    if (owner === undefined) {
+      getOwnerName(); // Fetch owner if not already fetched
+      return <p>Loading owner information...</p>;
     }
 
     return (
@@ -96,22 +123,41 @@ export default function HomePage() {
         <p>Your Balance: {balance}</p>
         <button onClick={deposit}>Deposit 1 ETH</button>
         <button onClick={withdraw}>Withdraw 1 ETH</button>
+        <div>
+          <label>Recipient Address: </label>
+          <input
+            type="text"
+            value={recipient}
+            onChange={(e) => setRecipient(e.target.value)}
+          />
+          <br />
+          <label>Transfer Amount: </label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <button onClick={transfer}>Transfer</button>
+        </div>
       </div>
-    )
-  }
+    );
+  };
 
-  useEffect(() => {getWallet();}, []);
+  useEffect(() => {
+    getWallet();
+  }, []);
 
   return (
     <main className="container">
-      <header><h1>Welcome to the Metacrafters ATM!</h1></header>
+      <header>
+        <h1>Welcome to the Metacrafters ATM!</h1>
+      </header>
       {initUser()}
       <style jsx>{`
-        .container {
-          text-align: center
-        }
-      `}
-      </style>
+          .container {
+            text-align: center;
+          }
+        `}</style>
     </main>
-  )
+  );
 }
