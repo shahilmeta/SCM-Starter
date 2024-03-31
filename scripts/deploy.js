5cf0f8e37499a5987ffb1efdd00be4710f95c20c
SURYAJIT ITHIPARAMBIL SUDHEERAN <124421567+Suryajit27@users.noreply.github.com>
2024-03-31 12:16:18 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,12 +7,11 @@
 const hre = require("hardhat");
 
 async function main() {
-  const initBalance = 1;
-  const Assessment = await hre.ethers.getContractFactory("Assessment");
-  const assessment = await Assessment.deploy(initBalance);
+  const Assessment = await hre.ethers.getContractFactory("SolidFundr");
+  const assessment = await Assessment.deploy();
   await assessment.deployed();
 
-  console.log(`A contract with balance of ${initBalance} eth deployed to ${assessment.address}`);
+  console.log(`A contract with balance of 0 eth deployed to ${assessment.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
